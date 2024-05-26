7bdff52ebc9e55535e769a2f9a244266fa15f295
inf2021211 <ioannaspinoula@gmail.com>
2024-05-26 14:12:26 +0300

    deletion of php

--- php/group_algorithms.php
+++
@@ -1,8 +0,0 @@
-<?php include_once 'header.php' ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<body class="index">
-machine learning algorithm for group
-</body>
-</html>
--- php/visualization.php
+++
@@ -1,8 +0,0 @@
-<?php include_once 'header.php' ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<body class="index">
-2d visualization of tabular data
-</body>
-</html>
